5d0436ea0e5363495bb3df10d608c09cc5ffce9d
Phillip Chadwick <phillipchadwick@hotmail.com>
2021-07-24 11:48:30 -0700

    adds xml and nullable explorations

---
+++ Explorations/Part3/nullables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// This class is here to illustrate that you can put a ? after a primative type to make it a nullable of that type
+/// This means a type like int that can't normally return null is able to return that type of value
+/// </summary>
+class nullables
+{
+    int? a = 123;
+    
+    static void setIntToNull(int? nullableINT)
+    {
+        a = null;
+    }
+    
+}
--- Explorations/Part3/xml.cs
+++ Explorations/Part3/xml.cs
@@ -2,36 +2,87 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
+using System.Linq;
+
 
 public static class Xml
 {
-    public static void xmlParse(string xmlString)
+    public static void xmlParse()
     {
-        XElement customer = XElement.Parse(xmlString);
+        XElement customer = XElement.Parse(createXml().ToString());
 
         foreach (var item in customer.Elements())
         {
             Console.WriteLine(item.Name + ": " + item.Value);
+            foreach (XAttribute attr in item.Attributes())
+            {
+                Console.WriteLine(attr.Name + ": " + attr.Value);
+            }
+        }
+        int count = customer.Elements("firstName").Count();
+        var name = customer.Elements().Where(e => e.Name == "firstName" && e.Value == "Joe");
+        Console.WriteLine("name: {0}", name.ToString());
+
+        foreach (XNode item in customer.Nodes())
+        {
+            // Saveoptions in this context is used to remove any formatting chars from the string like \n 
+            Console.WriteLine(item.ToString(SaveOptions.DisableFormatting));
+        }
+    }
+
+    public static void getInfoFromXml(XElement elements)
+    {
+        if (elements.HasAttributes)
+        {
+            foreach (var attribute in elements.Attributes())
+            {
+                Console.WriteLine("{0}: {1}", attribute.Name, attribute.Value);
+            }
+        }
+
+        if (elements.HasElements)
+        {
+            foreach (var element in elements.Elements())
+            {
+                getInfoFromXml(element);
+            }
         }
     }
 
     public static void xmlParseFromFile(string xmlDocumentPath)
     {
         XDocument xmlFile = XDocument.Load(xmlDocumentPath);
-        foreach (XElement element in xmlFile.Descendants("division"))
+
+        foreach (var item in xmlFile.Elements())
         {
-            Console.WriteLine(element);
+            getInfoFromXml(item);
         }
+    }
+
+    public static XElement createXml()
+    {
+        XElement lastName = new XElement("lastName", "jam");
+        lastName.Add(new XComment("nice name"));
+
+        XElement customer = new XElement("customer");
+        customer.Add(new XAttribute("id", 123));
+        customer.Add(new XElement("firstName", "Jim"));
+
+        customer.Add(lastName);
+
+        return customer;
     }
 
     public static void test()
     {
-        var xmlString = "<customer  id='123' status='active'><firstName>joe</firstName><lastName>Belfiore<!--nice name--></lastName></customer>";
-        var xmlFileLocation = "xml/baseball.xml";
+        var xmlFileLocation = "S:/VCC/client server computing/Explorations/Part3/xml/baseball.xml";
 
-        xmlParse(xmlString);
+        //xmlParse();
         xmlParseFromFile(xmlFileLocation);
     }
     
+    public static void xmlRetrieve()
+    {
 
+    }
 }
